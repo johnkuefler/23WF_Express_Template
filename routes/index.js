8a0c39de4ada7132843bc05a382a301d1b1ff23e
John Kuefler <john.kuefler@limelightmarketing.com>
2023-10-16 18:37:18 -0500

    added forms

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/create-todo', function(req, res, next) {
+  if (req.body.todoTitle === '' || req.body.todoDescription === '') {
+    res.send('Invalid request');
+  }
+
+  console.log(req.body.todoTitle.toUpperCase());
+  console.log(req.body.todoDescription.toUpperCase());
+
+  // save the todo data to a database
+
+  res.render('todo-created', {title: req.body.todoTitle.toUpperCase()});
+});
+
+router.post('/submit-patient', (req, res) => {
+  console.log(req.body);
+  res.send('Data received.');
+});
+
 module.exports = router;
